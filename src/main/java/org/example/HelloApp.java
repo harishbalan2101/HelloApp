f5922d4935afb2de7790be95db1eea56113b6542
harishbalan2101 <hb3398@srmist.edu.in>
2026-03-25 14:09:01 +0530

    usecase 3

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello, "+ name + "!");
-        }
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
     }
+}
 
