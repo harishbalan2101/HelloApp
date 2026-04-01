0ea9d5e80af8f112016734b28cf6ab0c438c2797
harishbalan2101 <hb3398@srmist.edu.in>
2026-04-02 02:09:33 +0530

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");   
     }
 }
