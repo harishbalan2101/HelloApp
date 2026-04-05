521d5eb99fb349801af0bb69a53a123a74fcf8bc
harishbalan2101 <hb3398@srmist.edu.in>
2026-04-05 18:03:22 +0530

    Update HelloApp.java

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,9 +1,14 @@
-package org.example;
+public class HelloApp{
+    public static void main (String[] args){
+        StringBuilder nameBuilder = new StringBuilder();
+        for (int i = 0;i < args.length;i++){
+            nameBuilder.append(args[i]);
+            if (i<args.length - 1){
+                nameBuilder.append(", ");
 
-public class HelloApp {
-    public static void main(String[] args) {
-        String name = (args.length > 0) ? args[0] : "World";
-        System.out.println("Hello, " + name + "!");
+            }
+        }
+        String name = nameBuilder.toString();
+        System.out.println(name);
     }
 }
-
